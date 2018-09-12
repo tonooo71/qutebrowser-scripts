1305d10f620768f63ff7c0dc7a6356de1d09909c
maruoka <mrtm.conv.tky@gmail.com>
2018-09-12 16:38:00 +0900

    Bug fixed

--- ejdict/ejdict.py
+++ ejdict/ejdict.py
@@ -9,31 +9,31 @@ with open(us_dir+'/ejdict/EJDict/src/ejdic-hand-utf8.txt') as lines:
     for line in lines:
         if line.startswith(word+'\t'):
             print(line)
-            break
+            sys.exit(0)
+with open(us_dir+'/ejdict/EJDict/src/ejdic-hand-utf8.txt') as lines:
+    for line in lines:
+        # 複数形, 三人称単数形
+        if word[-1] == 's':
+            if line.startswith(word[:-1]+'\t'):
+                print(line)
+                break
+        if word[-2:] == 'es':
+            if line.startswith(word[:-2]+'\t'):
+                print(line)
+                break
+        # 進行形
+        if word[-3:] == 'ing':
+            if line.startswith(word[:-3]+'\t'):
+                print(line)
+                break
+        # 過去形
+        if word[-1] == 'd':
+            if line.startswith(word[:-1]+'\t'):
+                print(line)
+                break
+        if word[-2:] == 'ed':
+            if line.startswith(word[:-2]+'\t'):
+                print(line)
+                break
     else:
-        for line in lines:
-            # 複数形, 三人称単数形
-            if word[-1] == 's':
-                if line.startswith(word[:-1]+'\t'):
-                    print(line)
-                    break
-            if word[-2:] == 'es':
-                if line.startswith(word[:-2]+'\t'):
-                    print(line)
-                    break
-            # 進行形
-            if word[-3:] == 'ing':
-                if line.startswith(word[:-3]+'\t'):
-                    print(line)
-                    break
-            # 過去形
-            if word[-1] == 'd':
-                if line.startswith(word[:-1]+'\t'):
-                    print(line)
-                    break
-            if word[-2:] == 'ed':
-                if line.startswith(word[:-2]+'\t'):
-                    print(line)
-                    break
-        else:
-            sys.exit(1)
+        sys.exit(1)
